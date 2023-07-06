dbe103c1c20c4d8432536485c92c505e334b38cb
unknown <tharindu24gayan@gmail.com>
2023-07-06 12:52:39 +0530

    creating lable adding and changing the text and set label font

--- src/ogr/tharindu/Main.java
+++ src/ogr/tharindu/Main.java
@@ -1,27 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ogr.tharindu;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.Group;
 import javafx.stage.Stage;
 
-/**
- *
- * @author HP
- */
 public class Main extends Application {
-    
+
     @Override
     public void start(Stage primaryStage) {
+        Group gr=new Group();
+
+        Label l1 = new Label("Welcome to javaWelcome to javaWelcome to javaWelcome to javaWelcome to javaWelcome to java");
+        l1.setPrefWidth(450);
+        l1.setPrefHeight(300);
+        l1.setAlignment(Pos.CENTER);
+        l1.setWrapText(true);
+        l1.setTextFill(Color.RED);
+        l1.setTooltip(new Tooltip("code amir JAVAFX is selected"));
+        l1.setFont(Font.font("Tahoma", FontWeight.BOLD, 48));
+
+        primaryStage.show();
         
+        gr.getChildren().add(l1);
+        Scene sc = new Scene(gr);
+        primaryStage.setScene(sc);
+        
+        primaryStage.setTitle("Code Amir");
+        primaryStage.setWidth(450);
+        primaryStage.setHeight(300);
         primaryStage.show();
     }
 
@@ -31,5 +45,5 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
